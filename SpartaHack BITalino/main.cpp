185875d1fdb795f15f7516af161c66b20fb0166d
cancui <can.cui@hotmail.com>
2016-02-27 17:53:51 -0500

    cleanup

--- SpartaHack BITalino/main.cpp
+++ SpartaHack BITalino/main.cpp
@@ -1,6 +1,5 @@
 
 #include "bitalino.h"
-
 #include <stdio.h>
 
 #ifdef _WIN32
@@ -40,13 +39,11 @@ bool keypressed(void)
 
 #define PRESSURE_SENSOR 3
 
-
 int initLength = 10;
 int touchSensorAverage = 0;
 BITalino::VFrame frames(10); //DEFAULTED TO 100!!! Check documentation
 BITalino::Frame* f;// = &frames[0];
 BITalino::Frame g;
-//BITalino::Frame f;
 
 bool updateTrigger = false;
 BITalino::Vbool outputs = { 0,0,0,0 };
@@ -84,78 +81,18 @@ void flashOutputs2(BITalino & bit) {
 	outputs = temp;
 }
 
-/*
-
-void checkFaceplant(BITalino & bit, int avg) {
-	BITalino::VFrame checker(100);
-	bool done = false;
-	while (!done)  {
-		bit.read(checker);
-		time_t start = clock();
-		while (abs(avg - checker[0].analog[PRESSURE_SENSOR]) > 100) {
-			bit.read(checker);
-			if (static_cast<double>(clock() - start) / CLOCKS_PER_SEC > 2) {
-				//std::thread t1(flashOutputs, bit); //flashOutputs(bit)
-				//t1.join();
-				flashOutputs(bit);
-				done = true;
-				//return;
-			}
-		}
-	}
-}
-
-void checkFaceplant2(BITalino & bit) {
-	bool done = false;
-	while (!done) {
-		int recentReading = f.analog[PRESSURE_SENSOR];
-		time_t start = clock();
-		while (abs(touchSensorAverage - recentReading) > 100) {
-			recentReading = f.analog[PRESSURE_SENSOR];
-			if (static_cast<double>(clock() - start) / CLOCKS_PER_SEC > 2) {
-				//std::thread t1(flashOutputs, bit); //flashOutputs(bit)
-				//t1.join();
-				flashOutputs2(bit);
-				done = true;
-				//break;?
-				//return;
-			}
-		}
-	}
-}
-
-*/
-
-void checkFaceplant3(BITalino & bit) {
-	bool done = false;
-	while (!done) {
-		int recentReading = f->analog[PRESSURE_SENSOR];
-		time_t start = clock();
-		while (abs(touchSensorAverage - recentReading) > 100) {
-			recentReading = f->analog[PRESSURE_SENSOR];
-			if (static_cast<double>(clock() - start) / CLOCKS_PER_SEC > 2) {
-				//std::thread t1(flashOutputs, bit); //flashOutputs(bit)
-				//t1.join();
-				flashOutputs2(bit);
-				done = true;
-				//break;//?
-				return;
-			}
-		}
-	}
-}
-
-void checkFaceplant4(BITalino & bit) {
-	//bool done = false;
+void checkFaceplant4(BITalino & bit) { 
 	while (true) {
 		int recentReading = g.analog[PRESSURE_SENSOR];
 		time_t start = clock();
-		while (abs(touchSensorAverage - recentReading) > 200) {
+		while (abs(touchSensorAverage - recentReading) > 100) { //trigger a thread?
 			recentReading = g.analog[PRESSURE_SENSOR];
 			if (static_cast<double>(clock() - start) / CLOCKS_PER_SEC > 4) {
+				//user has fallen asleep 
+				
 				//std::thread t1(flashOutputs, bit); //flashOutputs(bit)
 				//t1.join();
-				flashOutputs(bit);
+				flashOutputs(bit); //on a thread?
 				//done = true;
 
 				sleepCount++;
@@ -163,48 +100,11 @@ void checkFaceplant4(BITalino & bit) {
 				printf("%d \n", sleepCount);
 				printf("%d \n", sleepCount);
 				printf("%d \n", sleepCount);
-				wait(500); //pause not visable in consol because separate thread
-
-				//break;//?
-				
+				wait(500); //pause not visable in consol because separate thread		
 			}
 		}
 	}
 }
-
-
-void interfacer(BITalino & bit) { //For bridging thread. Thread updates sensor data, senses certain variables to trigger output
-	
-
-	//FRAMES DEFAULTED TO 100!!! Check documentation """"If the vector is empty, it is resized to 100 frames.""""
-
-	for (int i = 0; i < initLength; i++) {
-		bit.read(frames); 
-		touchSensorAverage += frames[0].analog[PRESSURE_SENSOR];
-		printf("%d\n", frames[0].analog[PRESSURE_SENSOR]);
-		wait(100);
-	}
-	touchSensorAverage = touchSensorAverage / initLength;
-	printf("\n %d", touchSensorAverage);
-
-	while (true) {
-		bit.read(frames);
-		f = &frames[0];
-
-		printf("%d : %d %d %d %d ; %d %d %d %d %d %d\n",  
-			f->seq,
-			f->digital[0], f->digital[1], f->digital[2], f->digital[3],
-			f->analog[0], f->analog[1], f->analog[2], f->analog[3], f->analog[4], f->analog[5]);
-		
-		if (updateTrigger) {
-			bit.trigger(outputs); //PROBLEM LINE, NOT TO BE CALLED EVERY TIME
-			//resize frames to size 10
-			updateTrigger = false;
-		}
-
-	}
-
-} 
 
 int main() {
    try {
@@ -217,37 +117,17 @@ int main() {
       std::string ver = dev.version();    // get device version string
       printf("BITalino version: %s\n", ver.c_str());
 
+	  printf("Maximum threads: %d\n", std::thread::hardware_concurrency());
+
       dev.battery(10);  // set battery threshold (optional)
       dev.start(1000, {0, 1, 2, 3, 4, 5});   // start acquisition of all channels at 1000 Hz
 	  // END OF PRE-INITIALIZATION
-
-	  /*
-	  int initLength = 10;
-	  int touchSensorAverage = 0;
-	  for (int i = 0; i < initLength; i++) {
-		  BITalino::VFrame frames(400);
-		  dev.read(frames);
-		  touchSensorAverage += frames[0].analog[PRESSURE_SENSOR];
-		  printf("%d\n", frames[0].analog[PRESSURE_SENSOR]);
-	  }
-	  touchSensorAverage = touchSensorAverage / initLength;
-	  printf("\n %d", touchSensorAverage);
-	  */
 
 	  flashOutputs(dev);
 
 	  dev.trigger({ 1,1,1,1 });
 	  std::string go; std::cin >> go;
 	  dev.trigger({ 0,0,0,0 });
-
-      //BITalino::VFrame frames(100); // initialize the frames vector with 100 frames 
-
-	  //checkFaceplant(dev, 220);
-	  //std::thread checkFaceplantThread(checkFaceplant, dev, touchSensorAverage);
-	  //checkFaceplantThread.join();
-
-	  
-
 
 	  for (int i = 0; i < initLength; i++) {
 		  dev.read(frames);
@@ -260,27 +140,13 @@ int main() {
 
 	  std::thread t1(checkFaceplant4, dev);
 	  
-	  do
-	  {
-		  dev.read(frames); // get 100 frames from device
-		  /*const BITalino::Frame & */f = &frames[0];  // get a reference to the first frame of each 100 frames block
+	  do {
+		  dev.read(frames); 
+		  f = &frames[0]; 
 		  g = frames[0]; 
 		  
-		  //printf("%d : %d %d %d %d ; %d %d %d %d %d %d\n",   // dump the first frame
-			 // f.seq,
-			 // f.digital[0], f.digital[1], f.digital[2], f.digital[3],
-			 // f.analog[0], f.analog[1], f.analog[2], f.analog[3], f.analog[4], f.analog[5]);
-
-
-
-
-		  //printf(/*%d : */ "%d %d %d %d ; %d %d %d %d %d %d\n",   // dump the first frame
-			 // //f->seq,
-			 // f->digital[0], f->digital[1], f->digital[2], f->digital[3],
-			 // f->analog[0], f->analog[1], f->analog[2], f->analog[3], f->analog[4], f->analog[5]);
-
-		  printf(/*%d : */ "%d %d %d %d ; %d %d %d %d %d %d\n",   // dump the first frame
-																  //f->seq,
+		  printf(/*%d : */ "%d %d %d %d ; %d %d %d %d %d %d\n",   
+			  //f->seq,
 			  g.digital[0], g.digital[1], g.digital[2], g.digital[3],
 			  g.analog[0], g.analog[1], g.analog[2], g.analog[3], g.analog[4], g.analog[5]);
 
@@ -292,12 +158,10 @@ int main() {
 
 	  } while (!keypressed()); // until a key is pressed
 
-	  //t1.join();
+	  t1.join();
 
       dev.stop(); // stop acquisition
-   } // dev is destroyed here (it goes out of scope)
-   catch (BITalino::Exception &e)
-   {
+   } catch (BITalino::Exception &e) {
       printf("BITalino exception: %s\n", e.getDescription());
    }
    
