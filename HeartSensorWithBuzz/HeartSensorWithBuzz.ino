a0d8b7d3f07af59e90d984568eb781bad637eb25
RAgarwal57 <rohanagarwal100@yahoo.com>
2016-02-27 20:29:29 -0500

    HeartSensorTingz

---
+++ HeartSensorWithBuzz/HeartSensorWithBuzz.ino
@@ -0,0 +1,66 @@
+ 
+void setup()
+{
+  Serial.begin(9600); 
+
+}
+
+void buzz()
+{
+  pinMode (1, OUTPUT);
+  long start = millis();
+  digitalWrite (1, HIGH);
+  while (millis() - start < 7500);
+  digitalWrite (1, LOW);
+}
+
+long averageBeats()
+{
+    int sensorValue = analogRead(A0);
+    long start;
+    long finish;
+    long time1; 
+    long start2;
+    long finish2;
+    long time2;
+    long start3;
+    long finish3;
+    long time3;
+    while (sensorValue <= 340);
+    start  = millis();
+    while (sensorValue >= 250);
+    finish = millis();
+    time1 = finish - start;
+
+    while (sensorValue <= 340);
+    start2  = millis();
+    while (sensorValue >= 250);
+    finish2 = millis();
+    time2 = finish2 - start2;
+
+
+    while (sensorValue <= 340);
+    start3  = millis();
+    while (sensorValue >= 250);
+    finish3 = millis();
+    time3 = finish3 - start3;
+
+
+    return (time1+time2+time3)/3;
+}
+ long originalBeats = averageBeats();
+void loop()
+{
+    
+    delay (180000); //3 minutes
+
+    if (abs(averageBeats()-originalBeats) > 300)
+    {
+      buzz;
+    }
+    
+      
+
+    
+}
+
